7748d49b480fa7c79be8d9f50916429a98398ea4
Juanes <johannstephan239@me.com>
2023-04-12 09:57:11 -0500

    Initial commit with the gpt-created files

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('register-btn').addEventListener('click', function() {
+    alert('Registration for the hackathon is now open. Please follow the instructions sent to your email.');
+});
