3c370e001e9de7d84061f69f7c968dd805bbd10f
Juanes <johannstephan239@me.com>
2023-04-12 21:37:41 -0500

    Fixed challenge 6 description, font sizes and remaining bullet

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('register-btn').addEventListener('click', function() {
-    alert('Registration for the hackathon is now open. Please follow the instructions sent to your email.');
-});
